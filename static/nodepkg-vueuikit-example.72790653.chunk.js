e4757f34fd65c21767f3b4f28d37a2c4b0b8dec8
morlay <morlay.null@gmail.com>
2023-03-31 14:06:19 +0000

    Deploying to gh-pages from @ innoai-tech/vuekit@bdfe384fc4583cd537447ebba10e677975b45301 🚀

--- static/nodepkg-vueuikit-example.72790653.chunk.js
+++
@@ -1,6 +0,0 @@
-import{c as t,g as o,j as r}from"./lib-nodepkg-vuekit.f20d97f1.chunk.js";import{T as a,B as s,s as d}from"./lib-nodepkg-vueuikit.d61f8b14.chunk.js";const e=d("p")`
-  a {
-    border-bottom: 1px solid currentColor;
-    cursor: pointer;
-  }
-`,n=t(()=>(console.log(a.use().colors),()=>o(s,{"data-theme":"dark",sx:{width:"20vw",border:"1px solid",borderRadius:"md",color:"body.text",bgColor:"body.bg",m:"10vh auto",px:"4"},children:[o(e,{class:"paragraph",children:["Some text. ",r("a",{children:"A link with a bottom border."})]}),o(e,{class:"paragraph",sx:{color:"red"},children:["Some text. ",r("a",{children:"A link with a bottom border."})]})]})));export{n as default};
