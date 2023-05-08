c59017ab184be8c1b54d4f1a01cfc380ae34cc46
Morlay <morlay.null@gmail.com>
2023-05-08 10:30:25 +0800

    docs(vuekit): added more testing

--- nodepkg/vuekit/src/__tests__/Provider.spec.tsx
+++ nodepkg/vuekit/src/__tests__/Provider.spec.tsx
@@ -2,16 +2,16 @@ import { test, describe, expect } from "vitest";
 import { component, createProvider } from "../index";
 import { mount } from "@vue/test-utils";
 import { t } from "@innoai-tech/typedef";
+import { ref, watch } from "vue";
 
 /**
  *  @vitest-environment jsdom
  **/
 describe("Provider", () => {
-  describe("standard", () => {
+  describe("static", () => {
     const P = createProvider(() => ({ context: "default" }));
     const C = component(({}, {}) => {
       const p = P.use();
-
       return () => <div>{p.context}</div>;
     });
 
@@ -24,15 +24,56 @@ describe("Provider", () => {
     test("could inject custom value", () => {
       const With = component(({}, {}) => {
         return () => (
-          <P value={{ context: "injected " }}>
+          <P value={{ context: "injected" }}>
             <C />
           </P>
         );
       });
 
-      const wrapper2 = mount(With, {});
-      expect(wrapper2.text()).toContain("injected");
-      wrapper2.unmount();
+      const wrapper = mount(With, {});
+      expect(wrapper.text()).toContain("injected");
+
+      wrapper.unmount();
+    });
+  });
+
+  describe("reactive", () => {
+    const P = createProvider(() => ref({ context: "default" }));
+    const C = component(({}, {}) => {
+      const p = P.use();
+      return () => <div>{p.value.context}</div>;
+    });
+
+    test("could use default value", () => {
+      const wrapper = mount(C, {});
+      expect(wrapper.text()).toContain("default");
+      wrapper.unmount();
+    });
+
+    test("could inject custom value", async () => {
+      const With = component({
+        context: t.string().optional().default("injected")
+      }, (props, {}) => {
+        const r = ref({ context: props.context });
+        watch(() => props.context, (context) => {
+          r.value = { context: context };
+        });
+
+        return () => (
+          <P value={r}>
+            <C />
+          </P>
+        );
+      });
+
+      const wrapper = mount(With, {});
+      expect(wrapper.text()).toContain("injected");
+      await wrapper.setProps({
+        context: "injected2"
+      });
+      expect(wrapper.text()).toContain("injected2");
+
+      wrapper.unmount();
     });
   });
 
@@ -55,7 +96,7 @@ describe("Provider", () => {
       wrapper.unmount();
     });
 
-    test("could inject custom value", () => {
+    test("could inject custom value", async () => {
       const With = component(({}, {}) => {
         return () => (
           <P input={"injected"}>
@@ -64,9 +105,63 @@ describe("Provider", () => {
         );
       });
 
-      const wrapper2 = mount(With, {});
-      expect(wrapper2.text()).toContain("injected");
-      wrapper2.unmount();
+      const wrapper = mount(With, {});
+      expect(wrapper.text()).toContain("injected");
+
+      wrapper.unmount();
+    });
+  });
+
+  describe("reactive factory", () => {
+    const P = createProvider(
+      {
+        input: t.string().default("default")
+      },
+      (props) => {
+        const r = ref(props.input);
+
+        watch(() => props.input, (input) => {
+          r.value = input;
+        });
+
+        return r;
+      }
+    );
+
+    const C = component(({}, {}) => {
+      const p = P.use();
+      return () => <div>{p.value}</div>;
+    });
+
+    test("could use default value", () => {
+      const wrapper = mount(C, {});
+      expect(wrapper.text()).toContain("default");
+      wrapper.unmount();
+    });
+
+    test("could inject custom value", async () => {
+      const With = component({
+        input: t.string().optional().default("injected")
+      }, (props, {}) => {
+        return () => (
+          <P input={props.input}>
+            <C />
+          </P>
+        );
+      });
+
+      const wrapper = mount(With, {});
+      expect(wrapper.text()).toContain("injected");
+
+
+      for (let i = 0; i < 2; i++) {
+        await wrapper.setProps({
+          input: "injected2"
+        });
+        expect(wrapper.text()).toContain("injected2");
+      }
+
+      wrapper.unmount();
     });
   });
 });
