056d7466e634ad5078dbcca6bb2852802ff1fb80
Morlay <morlay.null@gmail.com>
2023-08-22 17:05:05 +0800

    chore(deps): updates

--- nodepkg/vuemarkdown/src/Markdown.tsx
+++ nodepkg/vuemarkdown/src/Markdown.tsx
@@ -1,22 +1,25 @@
-import { rx, t, component$, type Component, type VNode } from "@innoai-tech/vuekit";
+import {
+  rx,
+  t,
+  component$,
+  type Component,
+  type VNode
+} from "@innoai-tech/vuekit";
 import { combineLatest, from, switchMap, map } from "rxjs";
 import { unified, remarkRehype, remarkParse, rehypeVue } from "./unified";
 
 export const Markdown = component$(
   {
     text: t.string(),
-    components: t
-      .record(t.string(), t.custom<Component<any>>())
-      .optional()
-      .default({}),
+    components: t.record(t.string(), t.custom<Component<any>>()).optional()
   },
   (props, { render }) => {
     const processor$ = props.components$.pipe(
       map((components) =>
         unified()
-          .use(remarkParse)
-          .use(remarkRehype)
-          .use(rehypeVue, { components: components })
+          .use(remarkParse as any)
+          .use(remarkRehype as any)
+          .use(rehypeVue, { components: components ?? {} })
       )
     );
 
--- nodepkg/vuemarkdown/src/rehypeVue.tsx
+++ nodepkg/vuemarkdown/src/rehypeVue.tsx
@@ -1,44 +1,21 @@
 import { type Component } from "@innoai-tech/vuekit";
-import { whitespace } from "hast-util-whitespace";
-// @ts-ignore
-import tableCellStyle from "@mapbox/hast-util-table-cell-style";
-import { h } from "vue";
-import { toH } from "hast-to-hyperscript";
-
-const tableElements = new Set(["table", "thead", "tbody", "tfoot", "tr"]);
+import { toJsxRuntime } from "hast-util-to-jsx-runtime";
+import { Fragment, jsx, jsxs } from "@innoai-tech/vuekit/jsx-runtime";
 
 export function rehypeVue({
-  components,
-}: {
+                            components
+                          }: {
   components: Record<string, Component<any>>;
 }) {
   // @ts-ignore
   Object.assign(this, { Compiler: compiler });
 
   function compiler(node: any) {
-    return toH(createElement, tableCellStyle(node));
-  }
-
-  function createElement(
-    name: string,
-    props: any,
-    children?: any[] | undefined
-  ) {
-    if (children && tableElements.has(name)) {
-      children = children.filter((child) => !whitespace(child));
-    }
-
-    if (components[name]) {
-      if (children) {
-        return h(components[name] as any, props, () => children);
-      }
-      return h(components[name] as any, props);
-    }
-
-    if (children) {
-      return h(name, props, children);
-    }
-
-    return h(name, props);
+    return toJsxRuntime(node, {
+      Fragment,
+      jsx,
+      jsxs,
+      components
+    });
   }
 }
