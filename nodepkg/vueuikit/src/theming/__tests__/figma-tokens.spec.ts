7056a001c49cd9ad01818a285132b2c4cc2bd0d8
Morlay <morlay.null@gmail.com>
2024-02-19 18:48:19 +0800

    feat(vueuikit): figma tokens updates

--- nodepkg/vueuikit/src/theming/__tests__/figma-tokens.spec.ts
+++ nodepkg/vueuikit/src/theming/__tests__/figma-tokens.spec.ts
@@ -1,29 +1,29 @@
-import { describe, expect, test } from "bun:test";
-import { Theming, defaultTheming, themeFromFigmaTokens } from "../index";
+import {describe, expect, test} from "bun:test";
+import {Theming, defaultTheming, themeFromFigmaTokens} from "../index";
 
 describe("#FigmaTokens", () => {
-	const figmaTokens = defaultTheming.toFigmaTokens();
+    const figmaTokens = defaultTheming.toFigmaTokens();
 
-	test("from figmaTokens", () => {
-		const themeTokens = themeFromFigmaTokens(
-			[figmaTokens.seed, figmaTokens.base],
-			[figmaTokens.dark],
-		);
+    test("from figmaTokens", () => {
+        const themeTokens = themeFromFigmaTokens(
+            [figmaTokens.seed, figmaTokens.base],
+            [figmaTokens.dark],
+        );
 
-		const t = Theming.create(
-			{
-				...defaultTheming.theme,
-				...themeTokens,
-			},
-			{
-				varPrefix: "vk",
-			},
-		);
+        const t = Theming.create(
+            {
+                ...defaultTheming.theme,
+                ...themeTokens,
+            },
+            {
+                varPrefix: "vk",
+            },
+        );
 
-		const nextTokens = t.toFigmaTokens();
+        const nextTokens = t.toFigmaTokens();
 
-		for (const k in nextTokens) {
-			expect((nextTokens as any)[k]).toEqual((figmaTokens as any)[k]);
-		}
-	});
+        for (const k in nextTokens) {
+            expect((nextTokens as any)[k]).toEqual((figmaTokens as any)[k]);
+        }
+    });
 });
--- nodepkg/vueuikit/src/theming/token/FigmaToken.ts
+++ nodepkg/vueuikit/src/theming/token/FigmaToken.ts
@@ -1,128 +1,168 @@
-import { has, isPlainObject, isString, mapValues } from "@innoai-tech/lodash";
-import { DesignToken } from "./DesignToken";
-import { setTo } from "./util.ts";
+import {get, has, isPlainObject, isString, last, mapValues, pickBy, some} from "@innoai-tech/lodash";
+import {DesignToken} from "./DesignToken";
+import {setTo} from "./util.ts";
 
 export type FigmaToken = {
-	type: string;
-	value: any;
+    $type: string;
+    $value: any;
 };
 
 export type FigmaTokenValues<T = FigmaToken> = {
-	[K: string]: FigmaToken | FigmaTokenValues<T>;
+    [K: string]: FigmaToken | FigmaTokenValues<T>;
 };
 
 export function themeFromFigmaTokens(
-	defaultTokens: FigmaTokenValues[],
-	darkTokens: FigmaTokenValues[],
+    defaultTokens: FigmaTokenValues[],
+    darkTokens: FigmaTokenValues[],
 ) {
-	const target: Record<string, any> = {};
+    const target: Record<string, any> = {};
 
-	walkFigmaTokens(defaultTokens, (v: any, path: string[]) => {
-		if (path[0] === "color" && path[1] === "sys") {
-			setTo(target, [...path, "_default"], processValue(v));
-			return;
-		}
-		setTo(target, path, processValue(v));
-	});
+    walkFigmaTokens(defaultTokens, (v: any, path: string[]) => {
+        if (path[0] === "color" && path[1] === "sys") {
+            setTo(target, ["color", "sys", last(path)!, "_default"], processValue(v));
+            return;
+        }
+        setTo(target, path, processValue(v));
+    });
 
-	walkFigmaTokens(darkTokens, (v: any, path: string[]) => {
-		if (path[0] === "color" && path[1] === "sys") {
-			setTo(target, [...path, "_dark"], processValue(v));
-			return;
-		}
-		setTo(target, path, processValue(v));
-	});
+    walkFigmaTokens(darkTokens, (v: any, path: string[]) => {
+        if (path[0] === "color" && path[1] === "sys") {
+            setTo(target, ["color", "sys", last(path)!, "_dark"], processValue(v));
+            return;
+        }
+        setTo(target, path, processValue(v));
+    });
 
-	const tokens: Record<string, DesignToken> = {};
+    const tokens: Record<string, DesignToken> = {};
 
-	for (const topic in target) {
-		switch (topic) {
-			case "fontWeight":
-				tokens[topic] = DesignToken.fontWeight(target[topic]);
-				break;
-			case "font":
-				tokens[topic] = DesignToken.font(target[topic]);
-				break;
-			case "shadow":
-				tokens[topic] = DesignToken.shadow(target[topic]);
-				break;
-			case "rounded":
-				tokens[topic] = DesignToken.rounded(target[topic]);
-				break;
-			case "color":
-				tokens[topic] = DesignToken.color(target[topic]);
-				break;
-			case "textStyle":
-				tokens[topic] = DesignToken.customMixin("textStyle", target[topic]);
-				break;
-		}
-	}
+    for (const topic in target) {
+        switch (topic) {
+            case "fontWeight":
+                tokens[topic] = DesignToken.fontWeight(target[topic]);
+                break;
+            case "font":
+                tokens[topic] = DesignToken.font(target[topic]);
+                break;
+            case "shadow":
+                tokens[topic] = DesignToken.shadow(target[topic]);
+                break;
+            case "rounded":
+                tokens[topic] = DesignToken.rounded(target[topic]);
+                break;
+            case "color":
+                tokens[topic] = DesignToken.color(target[topic]);
+                break;
+            case "textStyle":
+                tokens[topic] = DesignToken.customMixin("textStyle", target[topic]);
+                break;
+        }
+    }
 
-	return tokens;
+    return tokens;
 }
 
 function processValue(v: any): any {
-	if (isPlainObject(v)) {
-		return DesignToken.mixin(mapValues(v, (subV) => processValue(subV)));
-	}
+    if (isPlainObject(v)) {
+        return DesignToken.mixin(mapValues(v, (subV) => processValue(subV)));
+    }
 
-	if (isString(v)) {
-		if (v.includes("* {space.dp}")) {
-			return parseFloat(v.replaceAll("* {space.dp}", ""));
-		}
+    if (isString(v)) {
+        if (v.includes("* {space.dp}")) {
+            return parseFloat(v.replaceAll("* {space.dp}", ""));
+        }
 
-		if (v.startsWith("rgb")) {
-			return parseToRgb(v);
-		}
+        if (v.startsWith("rgb")) {
+            return parseToRgb(v);
+        }
 
-		return v.replace(/\{([^}]+)}/g, (v) => {
-			const scopeAndKey = v.slice(1, v.length - 1);
-			const [_, ...key] = scopeAndKey.split(".");
-			if (key.length) {
-				return key.join(".");
-			}
-			return v;
-		});
-	}
-	return v;
+        return v.replace(/\{([^}]+)}/g, (v) => {
+            const scopeAndKey = v.slice(1, v.length - 1);
+            const [_, ...key] = scopeAndKey.split(".");
+            if (key.length) {
+                return key.join(".");
+            }
+            return v;
+        });
+    }
+    return v;
 }
 
 function parseToRgb(color: string) {
-	const m = [...color.matchAll(/rgb\(([0-9]+), ?([0-9]+), ?([0-9]+)\)/g)][0];
+    const m = [...color.matchAll(/rgb\(([0-9]+), ?([0-9]+), ?([0-9]+)\)/g)][0];
 
-	if (!m) {
-		throw new Error(`invalid color: ${color}`);
-	}
+    if (!m) {
+        throw new Error(`invalid color: ${color}`);
+    }
 
-	// biome-ignore lint/style/noNonNullAssertion: <explanation>
-	return [parseInt(m[1]!), parseInt(m[2]!), parseInt(m[3]!)];
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    return [parseInt(m[1]!), parseInt(m[2]!), parseInt(m[3]!)];
 }
 
 function walkFigmaTokens(
-	tokens: FigmaTokenValues[],
-	cb: (value: any, path: string[]) => void,
+    tokens: FigmaTokenValues[],
+    cb: (value: any, path: string[]) => void,
 ) {
-	for (const token of tokens) {
-		walkValue(token, cb);
-	}
+    for (const token of tokens) {
+        walkValue(token, cb);
+    }
 }
 
 function walkValue(
-	o: any,
-	cb: (v: any, path: any[]) => void,
-	parent: any[] = [],
+    o: any,
+    cb: (v: any, path: any[]) => void,
+    parent: any[] = [],
 ) {
-	for (const k in o) {
-		const v = o[k] as any;
+    for (const k in o) {
+        const v = o[k] as any;
 
-		if (isPlainObject(v)) {
-			if (has(v, "type")) {
-				cb(v.value, [...parent, k]);
-				continue;
-			}
-			walkValue(v, cb, [...parent, k]);
-			continue;
-		}
-		cb(v, [...parent, k]);
-	}
+        if (isPlainObject(v)) {
+            if (has(v, "$type")) {
+                cb(v["$value"], [...parent, k]);
+                continue;
+            }
+            walkValue(v, cb, [...parent, k]);
+            continue;
+        }
+        cb(v, [...parent, k]);
+    }
 }
+
+
+const groups = {
+    "primary": [
+        "primary"
+    ],
+    "secondary": [
+        "secondary"
+    ],
+    "tertiary": [
+        "tertiary"
+    ],
+    "error": [
+        "error"
+    ],
+    "warning": [
+        "warning"
+    ],
+    "success": [
+        "success"
+    ],
+    "neutral": [
+        "surface",
+        "outline",
+    ],
+}
+
+export const groupSysColors = (figmaTokensValues: FigmaTokenValues) => {
+    const sysColors = get(figmaTokensValues, ["color", "sys"], {})
+
+    return {
+        ...figmaTokensValues,
+        color: {
+            sys: mapValues(groups, (matches) => {
+                return pickBy(sysColors, (_, token) => some(matches, (m) => token.indexOf(m) > -1))
+            })
+        }
+    };
+}
+
