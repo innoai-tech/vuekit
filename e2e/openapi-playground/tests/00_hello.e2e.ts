56608593e371e218db60ff1e8147f0912a4d9b5b
Morlay <morlay.null@gmail.com>
2025-10-20 18:05:13 +0800

    chore(dep): updates

--- e2e/openapi-playground/tests/00_hello.e2e.ts
+++ e2e/openapi-playground/tests/00_hello.e2e.ts
@@ -1,13 +1,22 @@
 import { expect, test } from "@playwright/test";
 
-test("openapi playground loaded and nav page", async ({ page }) => {
-  await page.goto("/api/example/_view");
+test("openapi playground", async ({ page }) => {
+  await test.step("when nav to home page, page should be loaded", async () => {
+    await page.goto("/api/example/_view");
+    await expect(page.locator(".OperationListItem").first()).toHaveAttribute(
+      "href",
+    );
 
-  await expect(page.locator(".OperationListItem").first()).toHaveAttribute(
-    "href",
-  );
+    await test.step("when click some operation, operation page should be naved", async () => {
+      await page.locator(".OperationListItem").first().click();
+      await expect(page.locator(".OperationHeading")).toContainText("/");
 
-  await page.locator(".OperationListItem").first().click();
-
-  await expect(page.locator(".OperationHeading")).toContainText("/");
+      await test.step("when click request, api should requested then show http response", async () => {
+        await page.locator('[type="submit"]').first().click();
+        await expect(page.locator(".ResponseSection pre")).toContainText(
+          "content-type",
+        );
+      });
+    });
+  });
 });
